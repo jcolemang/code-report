ee484cd8731f7483d544755f7176da950330337f
Coleman Gibson <gibsonjc@rose-hulman.edu>
2018-10-17 00:00:38 -0500

    Added more to the language

--- src/Lang/Lang.hs
+++ src/Lang/Lang.hs
@@ -2,6 +2,17 @@
 module Lang.Lang
 where
 
+data Formals
+  = Formals [String]
+  deriving ( Show )
+
+data LetPair
+  = LetPair String Lang
+  deriving ( Show )
+
 data Lang
   = Identifier String
+  | Lambda Formals Lang
+  | App Lang
+  | Let [LetPair] Lang
   deriving ( Show )
--- src/Lang/Parser.hs
+++ src/Lang/Parser.hs
@@ -8,6 +8,7 @@ import Lang.Lang
 
 import Data.Void
 import Control.Monad
+import Control.Applicative
 import qualified Text.Megaparsec as M
 import qualified Text.Megaparsec.Error as E
 import qualified Text.Megaparsec.Char.Lexer as L
@@ -38,17 +39,62 @@ parens = M.between (langSymbol "(") (langSymbol ")")
 
 -- runParseLang :: String -> Either (E.ParseError String Void) Lang
 runParseLang input =
-  M.parseMaybe (langWhitespace >> parseLang) input
-
+  M.parse (langWhitespace >> parseLang <* M.eof) "" input
 
 parseLang :: M.Parsec Void String Lang
 parseLang =
-  parseIdentifier
+  parseExpr
+
+parseExpr :: M.Parsec Void String Lang
+parseExpr =
+  parseVariable
+  <|> M.try parseLambda
+  <|> M.try parseApplication
+  <|> parseLet
   
-parseIdentifier :: M.Parsec Void String Lang
-parseIdentifier = do
-  iden <- langLexeme (M.some C.letterChar)
-  return (Identifier iden)
+parseIdentifier :: M.Parsec Void String String
+parseIdentifier =
+  langLexeme (M.some C.letterChar)
+
+parseVariable :: M.Parsec Void String Lang
+parseVariable = Identifier <$> parseIdentifier
+
+parseFormals :: M.Parsec Void String Formals
+parseFormals = do
+  formals <- M.some parseIdentifier
+  return $ Formals formals
+
+parseLambda :: M.Parsec Void String Lang
+parseLambda = parens $ do
+  _ <- langLexeme (C.string "lambda")
+  formals <- parens parseFormals
+  body <- parseExpr
+  return $ Lambda formals body
+
+parseApplication :: M.Parsec Void String Lang
+parseApplication = do
+  func <- parens parseExpr
+  return $ App func
+
+parseLetPair :: M.Parsec Void String LetPair
+parseLetPair = parens $ do
+  iden <- parseIdentifier
+  val <- parseExpr
+  return $ LetPair iden val
+
+parseLet :: M.Parsec Void String Lang
+parseLet = parens $ do
+  _ <- langLexeme (C.string "let")
+  pairs <- parens $ some parseLetPair
+  body <- parseExpr
+  return $ Let pairs body
+
+  
+  
+
+
+    
+  
 
 
   
